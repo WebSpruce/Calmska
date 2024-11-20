6c2899f1da3852e28c2c2ec09c2da08eb1a4e4f5
WebSpruce <kacperswi12363@gmail.com>
2024-11-20 20:51:26 +0100

    build: initialize .NET MAUI project and set up .gitignore Created base .NET MAUI project. Added .gitignore for excluding unnecessary files.

---
+++ Calmska/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Calmska
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
