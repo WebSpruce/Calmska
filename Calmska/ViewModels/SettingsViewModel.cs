3070fb4329ebb3edb19da82f82bb2dc2ad48349a
WebSpruce <kacperswi12363@gmail.com>
2025-02-11 20:34:40 +0100

    feat: settings ui Created UI for settings page that contains entry controls for username, working and break time.

--- Calmska/ViewModels/SettingsViewModel.cs
+++ Calmska/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace Calmska.ViewModels
+﻿using Calmska.Models.DTO;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text.Json;
+
+namespace Calmska.ViewModels
 {
-    internal class SettingsViewModel
+    internal partial class SettingsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string lUserName = string.Empty;
+        [ObservableProperty]
+        private string eUserName = string.Empty;
+
+        public SettingsViewModel()
+        {
+            string userJson = SecureStorage.Default.GetAsync("user_info").Result ?? string.Empty;
+            var user = JsonSerializer.Deserialize<AccountDTO>(userJson);
+            LUserName = user != null ? (user.UserName ?? string.Empty) : string.Empty;
+            EUserName = user != null ? (user.UserName ?? string.Empty) : string.Empty;
+        }
     }
 }
